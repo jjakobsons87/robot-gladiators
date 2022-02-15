b5f05cdef6c68a1c4e1db769c47c03de52655a05
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-02-14 20:39:01 -0500

    added .js commands

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// This is triggering a window to have the user type in the name of their robot
+var playerName = window.prompt("What is your robot's name?");
+// This is an alert that will pop up confirming the name of the robot the player chose
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message")
+
+console.log(10+10);
+
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight ();
