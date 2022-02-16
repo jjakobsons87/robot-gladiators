decb4d049c1c0546e6ff55d31018bff6567435b7
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-02-15 21:36:14 -0500

    add fighting logic and additional robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,8 +27,8 @@ var enemyAttack = 12;
 
 // creating the "fight" function 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round 
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
     // ask player if they'd like to fight or run 
     var promptFight = window.prompt("Do you want to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -44,8 +44,7 @@ var fight = function(enemyName) {
         // check enemy's health 
         if (enemyHealth <=0) {
             window.alert(enemyName + " has died.");
-        }
-        else {
+        } else {
             window.alert(enemyName + "still has " + enemyHealth + "health left.");
         }
 
@@ -58,13 +57,11 @@ var fight = function(enemyName) {
         //check players health 
         if(playerHealth <= 0) {
             window.alert(playerName + " has died!");
-        }
-        else {
+        } else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
-    }
-        // if player choses to skip 
-    else if (promptFight === "skip" || "SKIP") {
+    // if player choses to skip
+    } else if (promptFight === "skip" || "SKIP") {
         // confirm player wants to skip 
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -75,14 +72,20 @@ var fight = function(enemyName) {
             playerMoney = playerMoney - 2;
         }
 
-    //if no (false), ask question again by running fight() again
-    }
-    else {
-        fight();
-    }
+        //if no (false), ask question again by running fight() again
+        else {
+            fight();
+        }
+        } else {
+        window.alert("You need to pick a valid option. Try again!");
+  }
+};
 }
 
-//run fight function to start the game 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    //debugger;
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    // call the fight function with enemy-robot
+  fight(pickedEnemyName);
 }
